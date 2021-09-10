389c6928928ae898d84a37f19e2032e2c66cf820
Laura Fernandes Sorato <laurafernandessorato@gmail.com>
2021-09-10 20:51:41 -0300

    Adicionado activities a serem editadas

---
+++ app/src/main/java/com/example/antfyv2/AgendarActivity.java
@@ -0,0 +1,14 @@
+package com.example.antfyv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendar);
+    }
+}
