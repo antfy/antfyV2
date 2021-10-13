bb34f43da9e52119eb6ddceae93334bee12976eb
fslaurafs <laurafernandessorato@gmail.com>
2021-10-13 10:19:17 -0300

    Inicio Bluetooth

--- app/src/main/java/com/example/antfyv2/HomeActivity.java
+++ app/src/main/java/com/example/antfyv2/HomeActivity.java
@@ -23,28 +23,15 @@ import android.widget.Toast;
 
 import java.util.Objects;
 
-// LINK: https://www.youtube.com/watch?v=y0KNW133DY0
-// LINK: https://www.youtube.com/watch?v=x1y4tEHDwk0 (10:09)
-// https://github.com/devunwired/accessory-samples/tree/master/BluetoothGatt
-// LINK: https://medium.com/@shahar_avigezer/bluetooth-low-energy-on-android-22bc7310387a
+/*
+ * LINK TUTORIAL PORUGUÊS: https://www.youtube.com/watch?v=y0KNW133DY0
+ * LINK TUTORIAL INGLÊS: https://www.youtube.com/watch?v=x1y4tEHDwk0
+ * https://github.com/devunwired/accessory-samples/tree/master/BluetoothGatt
+ * https://medium.com/@shahar_avigezer/bluetooth-low-energy-on-android-22bc7310387a
+*/
 
 
 public class HomeActivity extends AppCompatActivity {
-
-    private static final String TAG = "BluetoohGattActivity";
-    private static final String DEVICE_NAME = "SensorTag";
-
-    // TEMPERATURE SERVICE
-    // HEART RATE SERVICE
-
-    private BluetoothAdapter mBluetoothAdapter;
-    private SparseArray<BluetoothDevice> mDevice;
-    private BluetoothGatt mConnectedGatt;
-
-    private TextView mTemperatura, mBatimento;
-
-    private ProgressDialog mProgress;
-
 
     @SuppressLint({"SetJavaScriptEnabled"})
     @Override
@@ -59,21 +46,6 @@ public class HomeActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOpcoes = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION + View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOpcoes);
-
-
-
-        mTemperatura = (TextView) findViewById(R.id.txtTemp);
-        mBatimento = (TextView) findViewById(R.id.txtBatimento);
-
-        // BLUETOOTH MANAGER
-        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-        mDevices = new SparseArray<BluetoothDevice>();
-
-        // PROGRESS
-        mProgress = new ProgressDialog(this);
-        mProgress.setIndeterminate(true);
-        mProgress.setCancelable(false);
-
 
 
         // SENTINDO ACTIVITY
@@ -102,55 +74,5 @@ public class HomeActivity extends AppCompatActivity {
         ImageView btPerfil = findViewById(R.id.btPerfil);
         btPerfil.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), PerfilActivity.class)));
     }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // verifica bluetooth habilitado
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            // bluetooth is disabled
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBtIntent);
-            finish();
-            return;
-        }
-
-        // verifica suporte para bluetooth low energy
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "No LE Support.", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-
-        clearDisplayValues();
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        // dialog hidden
-        mProgress.dismiss();
-
-        // cancel any scans in progress
-        mHandler.removeCallbacks(mStopRunnable);
-        mHandler.removeCallbacks(mStartRunnable);
-        mBluetoothAdapter.stopLeScan(this);
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // disconnect from any active tag connection
-        if (mConnectedGatt != null) {
-            mConnectedGatt.disconnect();
-            mConnectedGatt = null;
-        }
-    }
-
 
 }
